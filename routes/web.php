a5f2b041e5958049fabf23a7bbb89456a97c22e5
Landon Garwood <landongarwood6@gmail.com>
2022-07-28 02:41:39 -0500

    Add auth ui

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BooksController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('books', BooksController::class);
 });
